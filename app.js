856d87be434e1179a3540d9aee76e4a24f2096e3
Luca <lucagelmini@github.com>
2022-02-14 14:36:01 -0300

    Falta el ultimo modulo de posicionamiento

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const publicPath = path.resolve(__dirname,'public');
+app.use('/public', express.static(publicPath));
+
+app.get('/',(req, res)=> {
+    res.sendFile(path.join(__dirname, '/views/index.html'));
+});
+
+app.get('/register',(req, res)=> {
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+});
+app.get('/login',(req, res)=> {
+    res.sendFile(path.join(__dirname, '/views/login.html'));
+});
+
+app.listen(3030, ()=>{
+    console.log('Servidor corriendo')
+});
